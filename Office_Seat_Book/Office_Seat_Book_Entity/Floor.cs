1f2c86be85c250bda93c4f8d0f3f7a99c95e06e2
kiran <kiran.mh@valtech.com>
2023-04-04 11:09:02 +0530

    clone this

---
+++ Office_Seat_Book/Office_Seat_Book_Entity/Floor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Office_Seat_Book_Entity
+{
+    public class Floor
+    {
+        [Key]
+        public int FloorID { get; set; }
+
+        [AllowNull]
+        public string FloorName { get; set; }
+    }
+}
---
+++ Office_Seat_Book/Office_Seat_Book_Entity/Parking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Office_Seat_Book_Entity
+{
+    public class Parking
+    {
+        [Key]
+        public int ParkingID { get; set; }
+
+
+        [AllowNull]
+        public string ParkingType { get; set; }
+
+        [ForeignKey("booking")]
+        public int BookingID { get; set; }
+        public Booking booking { get; set; }
+
+    }
+}
